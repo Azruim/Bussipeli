7f6fda35da12ae695b175c5cf169bd92928e7798
MiikaViini <48917337+MiikaViini@users.noreply.github.com>
2019-04-02 14:29:15 +0300

    Uusi master 2.4.2019

---
+++ Bussipeli-master/Bussipeli/Assets/Our Files/Scripts/AreaManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AreaManager : MonoBehaviour
+{
+    public string otherArea;
+
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Player")
+        {
+            SceneManager.LoadScene(otherArea);
+        }
+    }
+}
