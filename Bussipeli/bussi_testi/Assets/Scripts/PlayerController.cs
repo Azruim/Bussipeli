56d698671bc95b3a73ab30164cd282bdcec3e685
Nico <32631608+Azruim@users.noreply.github.com>
2019-04-02 13:12:27 +0300

    bussi dialogi

---
+++ Bussipeli/bussi_testi/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float moveSpeed;
+
+    private Animator anim;
+    private bool playerMoving;
+    private Vector2 lastMove;
+    private GameObject dialogue;
+
+    //estää hahmon controllit
+    public static bool movementOn = true;
+
+    public bool isInTrigger;
+    public bool triggered;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        triggered = false;
+        playerMoving = false;
+
+        if (movementOn)
+        {
+            if (Input.GetAxisRaw("Horizontal") > 0.5f || Input.GetAxisRaw("Horizontal") < -0.5f)
+            {
+                transform.Translate(new Vector3(Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime, 0f, 0f));
+                playerMoving = true;
+                lastMove = new Vector2(Input.GetAxisRaw("Horizontal"), 0f);
+            }
+
+            if (Input.GetAxisRaw("Vertical") > 0.5f || Input.GetAxisRaw("Vertical") < -0.5f)
+            {
+                transform.Translate(new Vector3(0f, Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime, 0f));
+                playerMoving = true;
+                lastMove = new Vector2(0f, Input.GetAxisRaw("Vertical"));
+            }
+
+            if (Input.GetKeyUp("space") && isInTrigger)
+            {
+                triggered = true;
+                if (dialogue != null)
+                {
+                    movementOn = false;
+                    dialogue.GetComponent<DialogueTrigger>().TriggerDialogue();
+                    dialogue = null;
+                }
+                Debug.Log("Triggered!!");
+            }
+        }
+
+        anim.SetFloat("MoveX", Input.GetAxisRaw("Horizontal"));
+        anim.SetFloat("MoveY", Input.GetAxisRaw("Vertical"));
+        anim.SetBool("PlayerMoving", playerMoving);
+        anim.SetFloat("LastMoveX", lastMove.x);
+        anim.SetFloat("LastMoveY", lastMove.y);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Trigger")
+        {
+            isInTrigger = true;
+            dialogue = collision.gameObject;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Trigger")
+        {
+            isInTrigger = false;
+
+        }
+    }
+}
+    
+
