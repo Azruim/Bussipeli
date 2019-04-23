1b76a32d5515396926d47980261b520f0fa774e2
MiikaViini <48917337+MiikaViini@users.noreply.github.com>
2019-04-23 13:31:21 +0300

    Demo 23.4.2019

---
+++ demo/Assets/Our Files/Scripts/GameStatus.cs
@@ -0,0 +1,158 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameStatus : MonoBehaviour
+{
+    //SINGLETON:
+    //"Singleton" variable. Used to make sure we always have only one GameStatus and to know which we want to exist at all times.
+    static GameStatus trueOne;
+
+    //PLAYER INFO:
+    //Vector3 used to know the players position and to also set it.
+    private Vector3 playerPosition;
+
+    //MINIGAMES:
+    //Booleans for minigames, so that GameStatus knows if they've been played. Also used to stop the player from replaying those games.
+    private bool sockGamePlayed;
+    private bool toiletPaperGamePlayed;
+
+    //TIMER:
+    //Variables for our clock timer
+    private int minTime;
+    private int secTime;
+    //Boolean that is used to freeze our timer when wanted.
+    private bool timerOn;
+    //Used to count 1 second.
+    private float oneSecondCheck;
+
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        //SINGLETON & DON'T DESTROY ON LOAD:
+        //Used to check if the GameStatus is the one we want to exist.
+        if (trueOne != null)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+        trueOne = this; //We set our GameStatus as the one we want to exist.
+        GameObject.DontDestroyOnLoad(this.gameObject); //We tell Unity that we don't want this GameObject to be destroyed.
+
+        //PLAYER INFO:
+        playerPosition = new Vector3(-4.5f, -1.9f, 0.0f); //We set the starting coordinates for our player character, when the game starts.
+        
+        //MINIGAMES:
+        //We set minigames to be NOT played by default at the start of the game.
+        sockGamePlayed = false;
+        toiletPaperGamePlayed = false;
+
+
+        //TIMER:
+        //We set our countdown time variables to be 2 minutes by default.
+        minTime = 2;
+        secTime = 0;
+        //We set timerOn, so that the countdown moves by default.
+        timerOn = true;
+        //We set our second counter to start from 0.
+        oneSecondCheck = 0.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Debug.Log(sockGamePlayed);
+
+        //TIMER:
+        if(timerOn) //If true, our timer moves.
+        {
+            MoveTime(); //Calls for our timer and makes it move.
+        }
+        
+
+    }
+    //PLAYER INFO:
+    //Used to GET players position, so that when we switch scene back to the overworld, we can put the players to the last position they were on.
+    public Vector3 GetPlayerPosition()
+    {
+        return playerPosition;
+    }
+    //Used to SET players position, so that when we switch scenes, we can save the players position.
+    public void SetPlayerPosition(Vector3 newPosition)
+    {
+        playerPosition = newPosition;
+    }
+   
+    //MINIGAMES:
+    //Used to GET a specific boolean for a minigame.
+    public bool GetMiniGamePlayed(string miniGameName)
+    {
+        if(miniGameName == "sockGame")
+        {
+            return sockGamePlayed;
+        } else if (miniGameName == "toiletPaperGame")
+        {
+            return toiletPaperGamePlayed;
+        }
+        return true;
+    }
+    //Used to SET a specific boolean for a minigame.
+    public void SetMiniGamePlayed(string miniGameName, bool setter)
+    {
+        if (miniGameName == "sockGame")
+        {
+            sockGamePlayed = setter;
+        } else if (miniGameName == "toiletPaperGame")
+        {
+            toiletPaperGamePlayed = setter;
+        }
+    }
+
+    //TIMER:
+    //Moves the timer "forward".
+    private void MoveTime()
+    {
+        oneSecondCheck += Time.deltaTime; //We count a second.
+
+        if ((int)oneSecondCheck == 1) //If 1 second has passed, we execute and add 1 second to our countdown timer.
+        {
+            oneSecondCheck = 0.0f;
+            secTime -= 1;
+            if (secTime == -1) //If seconds go lower than 0, we execute and set seconds to 59 and decrease minutes by 1.
+            {
+                secTime = 59;
+                minTime -= 1;
+            }
+        }
+        Debug.Log(minTime + ":" + secTime); //Used to check that time flows correctly through console.
+    }
+    
+    public int GetSeconds() //GETs the timer seconds.
+    {
+        return secTime;
+    }
+    public void SetSeconds(int setter) //SETs the timer seconds.
+    {
+        secTime = setter;
+    }
+    public int GetMinutes() //GETs the timer minutes.
+    {
+        return minTime;
+    }
+    public void SetMinutes(int setter) //SETs the timer minutes.
+    {
+        minTime = setter;
+    }
+
+    public bool GetTimerOn() //GETs the boolean for timerOn, which is used to freeze the time by changing it to "false".
+    {
+        return timerOn;
+    }
+    public void SetTimerOn(bool setter) //SETs the boolean for timerOn, which is used to freeze the time by changing it to "false".
+    {
+        timerOn = setter;
+    }
+
+
+}
---
+++ demo/Assets/Our Files/Scripts/PlayerController.cs
@@ -0,0 +1,175 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerController : MonoBehaviour
+{
+    //Player animation related variables
+    private Animator anim;
+    private bool playerMoving;
+    private Vector2 lastMove;
+    private GameObject dialogue;
+
+    public bool isInTrigger;
+    public bool triggered;
+
+    //Reseting game varables
+    private bool resetGame;
+    private float resetCounterTime;
+    private float timeItTakesToReset = 3.0f;
+    private string beginningLevel = "main";
+
+
+    //Movement variables
+    public float speed = 1.0f;
+    private Rigidbody2D rb2d;
+    public Vector2 movement;
+
+    //Used to freeze players controls
+    public static bool movementOn = true;
+
+    private Collider2D sideCollider;
+    private Collider2D frontCollider;
+
+    //Vector3 that used just incase of problems
+    private Vector3 checkVector;
+    //variable for GameStatus component
+    private GameStatus gameStatus;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = this.GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+
+        //gets box collider 2d from child SideCollision
+        sideCollider = transform.Find("SideCollision").gameObject.GetComponent<BoxCollider2D>();
+        //gets Players Box collider 2D
+        frontCollider = GetComponent<BoxCollider2D>();
+        //Disables players other collider 
+        frontCollider.enabled = false;
+
+        //Vector with 0.0f in all axis, used to that the player will start in the right position
+        checkVector = new Vector3(0.0f,0.0f,0.0f);
+        //Gets the gamestatus component
+        gameStatus = GameObject.Find("GameStatus").GetComponent<GameStatus>();
+        
+        //Debug.Log(gameStatus.GetPlayerPosition());
+        //if the GameStatus, for whatever reason, hasn't set a coordinates for playerpostion, this is used as a failsafe
+        if(gameStatus.GetPlayerPosition() == checkVector)
+        {
+            this.transform.position = new Vector3(-4.5f, -1.9f, 0.0f);
+        }
+        this.transform.position = gameStatus.GetPlayerPosition();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        triggered = false;
+        playerMoving = false;
+
+        //Player movement on/off checker
+        if (movementOn)
+        {
+            //Players movement Input
+            movement = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+            
+            //Players Animator inputs 
+            if(Input.GetAxisRaw("Horizontal") > 0.5f || Input.GetAxisRaw("Horizontal") < -0.5f)
+            {
+                playerMoving = true;
+                sideCollider.enabled = true;
+                frontCollider.enabled = false;
+                lastMove = new Vector2(Input.GetAxisRaw("Horizontal"), 0f);
+            } else if (Input.GetAxisRaw("Vertical") > 0.5f || Input.GetAxisRaw("Vertical") < -0.5f)
+            {
+                playerMoving = true;
+                sideCollider.enabled = false;
+                frontCollider.enabled = true;
+                lastMove = new Vector2(0f, Input.GetAxisRaw("Vertical"));
+            }
+
+            //Player Space button input
+            if (Input.GetKeyUp("space") && isInTrigger)
+            {
+                triggered = true;
+                if (dialogue != null)
+                {
+                    movementOn = false;
+                    dialogue.GetComponent<DialogueTrigger>().TriggerDialogue();
+                    dialogue = null;
+                }
+                //Debug.Log("Triggered!!");
+            }
+        } else
+        {
+            movement = new Vector2(0.0f, 0.0f); //Makes sure that when we feeze the players controls, that they stop moving and won't slide around.
+        }
+        
+        //Animator guidelines
+        anim.SetFloat("MoveX", Input.GetAxisRaw("Horizontal"));
+        anim.SetFloat("MoveY", Input.GetAxisRaw("Vertical"));
+        anim.SetBool("PlayerMoving", playerMoving);
+        anim.SetFloat("LastMoveX", lastMove.x);
+        anim.SetFloat("LastMoveY", lastMove.y);
+
+        //Resetting game by pressing R-button for 3 seconds
+        if (Input.GetKey(KeyCode.R))
+        {
+            resetCounterTime += Time.deltaTime;
+            if (resetCounterTime >= timeItTakesToReset)
+            {
+                resetGame = true;
+            }
+        }
+        else
+        {
+            resetCounterTime = 0.0f;
+            resetGame = false;
+        }
+        if (resetGame)
+        {
+            SceneManager.LoadScene(beginningLevel);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        MoveCharacter(movement);
+    }
+
+    //Player Movement method
+    void MoveCharacter(Vector2 direction)
+    {
+        rb2d.MovePosition((Vector2)transform.position + (direction * speed * Time.deltaTime));
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Trigger")
+        {
+            isInTrigger = true;
+            dialogue = collision.gameObject;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Trigger")
+        {
+            isInTrigger = false;
+
+        }
+    }
+
+    private void OnDestroy()
+    {
+        //Debug.Log("I was destroyed");
+        gameStatus.SetPlayerPosition(this.transform.position); //When the player gets destroyed, we send the players coordinates to the GameStatus.
+    }
+}
+    
+
