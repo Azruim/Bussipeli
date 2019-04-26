e63088c7518cb76d026697653f345de1d08bb214
Nico <32631608+Azruim@users.noreply.github.com>
2019-04-26 14:49:02 +0300

    uus demo

---
+++ Bussipeli-Not-Master-Jonathan/demo copy/Assets/Our Files/Scripts/DisplayPlayerPoints.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DisplayPlayerPoints : MonoBehaviour
+{
+    public Text playerPointsText;
+    private GameStatus gameStatus;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerPointsText = gameObject.GetComponent<Text>();
+        gameStatus = GameObject.Find("GameStatus").GetComponent<GameStatus>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        playerPointsText.text = "Points: " + gameStatus.GetPlayerPoints();
+    }
+}
---
+++ Bussipeli-Not-Master-Jonathan/demo copy/Assets/Our Files/Scripts/TemporaryStoreTrigger.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TemporaryStoreTrigger : MonoBehaviour
+{
+
+    private bool playerInTrigger;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerInTrigger = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(playerInTrigger && Input.GetKeyDown(KeyCode.Space))
+        {
+            SceneManager.LoadScene("FridgeStore");
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            playerInTrigger = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            playerInTrigger = false;
+        }
+    }
+}
